71ece3e18149ec64a9f1a4d4b83d499bbcd6441a
JoseBorda <bordajoseph4@gmail.com>
2025-06-18 05:49:20 -0500

    subiendo archivos de practica js

---
+++ index.js
@@ -0,0 +1,43 @@
+// let nombre = 'Admin el mejor un grande';
+// let admin = nombre;
+
+// console.log(admin);
+
+// var tierra = 'Tierra';
+// var sitioWeb = 'sitioweb@gmail.com';
+
+// console.log(sitioWeb);
+
+//const birthday = '28.08.2002';
+
+// const age = someCode(birthday);
+
+// console.log(age);
+
+// prompt('Esto es una prueba de prompt', birthday);
+
+/*let numero1 = prompt('Digite numero 1: ');
+let numero2 = prompt('Digite numero 2: ');
+
+total = parseInt(numero1) + parseInt(numero2)
+;
+
+alert('La suma de los dos numeros es: ' + total);*/
+
+// var numero = 5;
+// var string = 'Hola Mundo';
+// var decimal = 5.5; // apesar de tener un punto para el decimal sigue siendo de tipo number
+
+// alert(typeof numero);
+// alert(typeof string);
+// alert(typeof decimal);
+
+// if (confirm('Quiere ser mi novia?')) {
+//     alert('Ahora somos novios');
+// } else {
+//     alert('Te lo vuelvo a preguntar');
+//     if (confirm('Quiere ser mi novia?')) {
+//         alert('Muy bien, asi debe ser');
+//     }
+// }
+
